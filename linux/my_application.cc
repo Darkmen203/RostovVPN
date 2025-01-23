d16926ab8181e7ef179f6fe23e3c27eeb46814b3
Darkmen203 <dima-pivovarov03@mail.ru>
2025-01-23 15:01:57 +0300

    fix name on linux icon

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -14,7 +14,7 @@ struct _MyApplication
 };
 
 G_DEFINE_TYPE(MyApplication, my_application, GTK_TYPE_APPLICATION)
-#define ICON_PATH "./rostovvpn.png"
+#define ICON_PATH "./rostov_vpn.png"
 
 // Implements GApplication::activate.
 static void my_application_activate(GApplication *application)
